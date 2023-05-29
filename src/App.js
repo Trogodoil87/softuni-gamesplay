180ef8f7a82b580b55da37139cd053aa77097d15
Trogodoil87 <encho871337@gmail.com>
2023-05-29 14:30:19 +0300

    Add React router to project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { NavBar } from './components/Naviagtion';
+import { Route, Routes } from 'react-router-dom'
+
+import { Header } from './components/Header/Header';
 import { Home } from './components/home/Home';
 import { Login } from './components/login/Login'
 import { Register } from './components/register/Register';
@@ -12,10 +14,11 @@ import './App.css';
 function App() {
   return (
     <div id="box">
-      <NavBar />
-      {/* Main Content */}
+      <Header />
       <main id="main-content">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </main>
     </div>
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-export const NavBar = () => {
+export const Header = () => {
     return (
         <header>
         {/* Navigation */}
